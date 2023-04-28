14996bc5e0d20f5e02347e9b03223d7ed962752c
Jonathan Cordoba <jhonathan.cordoba@oracle.com>
2023-04-28 15:48:46 -0500

    Index text modified

--- app/index.php
+++ app/index.php
@@ -36,7 +36,7 @@
 </head>
 <body>
 	<div class="heading">
-		<h2 style="font-style: 'Hervetica';">ToDo List Application</h2>
+		<h2 style="font-style: 'Hervetica';">ToDo List Application (Virtual Nodes test)</h2>
     </div>
     <div class="heading">    
         <h3 style="font-style: 'Hervetica'; margin: 10px">
