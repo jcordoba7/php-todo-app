1fc6d4e22126884f2d3dc61189585c4bb4c77bb0
Jonathan Cordoba <jhonathan.cordoba@oracle.com>
2023-07-18 10:40:40 -0500

    Date updated

--- app/index.php
+++ app/index.php
@@ -36,7 +36,7 @@
 </head>
 <body>
 	<div class="heading">
-		<h2 style="font-style: 'Hervetica';">ToDo List Application (18-julio-2023  -  10:20 am)</h2>
+		<h2 style="font-style: 'Hervetica';">ToDo List Application (10-julio-2023  -  10:30 am)</h2>
     </div>
     <div class="heading">    
         <h3 style="font-style: 'Hervetica'; margin: 10px">
