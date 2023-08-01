4e6ca143a825c0feb6d4405dc985b3f510f14f55
Jonathan Cordoba <jhonathan.cordoba@oracle.com>
2023-08-01 17:04:45 -0500

    Restauración a despliegue simple

--- app/index.php
+++ app/index.php
@@ -36,7 +36,7 @@
 </head>
 <body>
 	<div class="heading">
-		<h2 style="font-style: 'Hervetica';">ToDo List Application (01-agosto-2023  -  11:15 am)</h2>
+		<h2 style="font-style: 'Hervetica';">ToDo List Application (01-agosto-2023  -  05:10 pm)</h2>
     </div>
     <div class="heading">    
         <h3 style="font-style: 'Hervetica'; margin: 10px">
