3ea26780ac7c90e03a1b17d68dac2ebd85d34a51
Jonathan Cordoba <jhonathan.cordoba@oracle.com>
2023-09-04 15:37:05 -0500

    Cambio en el style css a blue

--- app/index.php
+++ app/index.php
@@ -32,11 +32,11 @@
 <html>
 <head>
 	<title>ToDo List Application PHP and MySQL</title>
-    <link rel="stylesheet" type="text/css" href="style-green.css">
+    <link rel="stylesheet" type="text/css" href="style-blue.css">
 </head>
 <body>
 	<div class="heading">
-		<h2 style="font-style: 'Hervetica';">ToDo List Application (01-agosto-2023  -  05:10 pm)</h2>
+		<h2 style="font-style: 'Hervetica';">ToDo List Application (04-septiembre-2023  -  35:42 pm)</h2>
     </div>
     <div class="heading">    
         <h3 style="font-style: 'Hervetica'; margin: 10px">
