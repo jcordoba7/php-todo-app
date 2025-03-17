772781209abbbb22751dd2943674ae36815f44bd
Jonathan Cordoba <jhonathan.cordoba@oracle.com>
2025-03-17 11:36:10 -0500

    Cambio a amarillo.

--- app/index.php
+++ app/index.php
@@ -32,7 +32,7 @@
 <html>
 <head>
 	<title>ToDo List Application PHP and MySQL</title>
-    <link rel="stylesheet" type="text/css" href="style-green.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="heading">
